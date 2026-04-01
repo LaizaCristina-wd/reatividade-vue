4808fa0f7039e4bed942e701c9cf30f992f24326
laiza <laizacristina2507@gmail.com>
2026-04-01 16:37:26 -0300

    Atividade guiada Vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import UserSection from './components/UserSection.vue'
+import CounterSection from './components/CounterSection.vue'
+import ComunicationSection from './components/ComunicationSection.vue'
+import LifecycleSection from './components/LifecycleSection.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  
+  <div class="container py-4">
+        <h1 class="text-center text-success mb-4">Reactivity in Vue 3</h1>
+  </div>
+  <counter-section />
+  <user-section />
+  <ComunicationSection />
+  <LifecycleSection />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/LifecycleLog.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onUnmounted } from 'vue'
+
+const emit = defineEmits(['log'])
+
+const clicks = ref(0)
+
+onBeforeMount(() => emit('log', 'onBeforeMount'))
+onMounted(() => emit('log', 'onMounted'))
+onBeforeUpdate(() => emit('log', 'onBeforeUpdate'))
+onUpdated(() => emit('log', 'onUpdated'))
+onUnmounted(() => emit('log', 'onUnmounted'))
+</script>
+
+<template>
+  <div class="border rounded p-3">
+        <p class="mb-1">Clicks: <strong class="text-success">{{ clicks }}</strong></p>
+        <button class="btn btn-success btn-sm" @click="clicks++">Click here</button>
+        <p class="text-muted small mt-2 mb-0">Click to trigger onBeforeUpdate / onUpdated</p>
+  </div>
+</template>
+
+<style scoped>
+.border {
+  border-color: #198754 !important;
+}
+</style>
